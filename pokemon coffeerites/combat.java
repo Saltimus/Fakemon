f73a523bc6192d7dc96ca85444f85f192f83aa43
Irvin <benomonster1423@gmail.com>
2016-10-23 17:26:17 +0800

    HELP
    try catch problem looping like a christmas tree

--- pokemon coffeerites/combat.java
+++ pokemon coffeerites/combat.java
@@ -1,27 +1,32 @@
-public class combat extends pokeDex{
+import java.util.Scanner;
+class combat{
    
-   static Scanner n= new Scanner(System.in);
 	public static void main(String [] args){
+
+   Scanner no= new Scanner(System.in);
    
    int limit=1;
-   String pchoice;
+   int pchoice=0;
    String name;
    int hp,def,spd;
+   int limita=0;
+   int n=0;
+   int i=2;
+   int ni=0;
+   boolean yeah= true;
 
+   String player[] = new String[i]; 
    
-   player players[]= new player[2]; 
-   
-   players[0] = new player();
-   players[1] = new player();
+   player[0] = "Player 1" ;
+   player[1] = "Player 2" ;
    
    
    System.out.println("\t\tWelcome to Pokemon Beta");
    System.out.println();
    
-   do{
-   if(limit==1 || limit==2){
-      if(limit==1){
-      System.out.println("Hello Player 1");
+   for(limit=2 ; limit>=1; limit--){
+   
+      System.out.println("Hello" + player[ni++]);
       System.out.println("Pick your pokemon");
       System.out.println("1. Charmander");
       System.out.println("2. Squirtle");
@@ -29,45 +34,52 @@ public class combat extends pokeDex{
       System.out.println("4. Cyndaquil");
       System.out.println("5. Machop");
       System.out.println("------------------");
-   try{
-      System.out.print("Pokemon Choice:");
-      pchoice= n.nextLine();
-   }catch(Exception e){
-   System.out.println("--**Choice not Found--");
-   input.next();
-   }
-     do{ 
+       
+        do{
+            try{
+                System.out.print("Pokemon Choice:");
+                pchoice= no.nextInt();
+                yeah=false;
+            }catch(Exception e)
+             {
+             System.out.println("--Invalid Input**");
+               }
+        }while(yeah);
+        
+   }   
          switch(pchoice){
-         case '1':
+         case 1:
+            limita=1;
             System.out.println("Charmander");
-            pokeDex.pDex_array[0];
+
             break;
-         case '2':
+            
+         case 2:
+            limita=1;
             System.out.println("Squirtle");
+
             break;
-         case '3':
+            
+         case 3:
+            limita=1;
             System.out.println("Pikachu");
+
             break;
-         case '4':
+            
+         case 4:
+            limita=1;
             System.out.println("Cyndaquil");
+
             break;
-         case '5':
+            
+         case 5:
+            limita=1;
             System.out.println("Machop");
+
             break;
+            
          default:
-            System.out.println("--Invalid Input--");
+            System.out.println("--go die--");
           }
-      }while();
-    
-      }
-      else if(limit==2){
-      System.out.println("Hello Player 2");
-      }
-   } 
-    else;
-    
-   
-   }while(limit!=2);
-   
-   }
+}
 }
